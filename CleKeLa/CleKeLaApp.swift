03ae85569b840ec24e6cb2e868c97f179524365c
Axel Lalaut <axel.lalaut@gmail.com>
2026-05-29 12:12:35 +0200

    push

--- CleKeLa/CleKeLaApp.swift
+++ CleKeLa/CleKeLaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CleKeLaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Accueil()
         }
     }
 }
--- CleKeLa/ContentView.swift
+++ CleKeLa/ContentView.swift
@@ -7,18 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Accueil: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    NavigationLink(destination: Note(titre: "Mon plat préféré")) {
+                        Text("Mon plat préféré")
+                    }
+                    NavigationLink(destination: Note2()) {
+                        Text("Comment avoir 1 million d'euros ?")
+                    }
+                } header: {
+                    Text("Notes les plus récentes")
+                } footer: {
+                    Text("Autres notes")
+                }
+            }
+            .environment(\.defaultMinListHeaderHeight, 100)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+// Première note
+struct Note: View {
+    let titre: String
+    var body: some View {
+        Text(titre)
+            .navigationTitle("Mon plat préféré")
+    }
+}
+
+// Deuxième note
+struct Note2: View {
+    var body: some View {
+        Text("Travailler dur, économiser…")
+            .navigationTitle("Comment avoir 1 million d'euros")
+    }
 }
