4d86cdff3bc9de5ac12bcae959df5a41d22f8eeb
Axel Lalaut <axel.lalaut@gmail.com>
2026-05-29 11:33:42 +0200

    Initial Commit

---
+++ CleKeLa/CleKeLaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleKeLaApp.swift
+//  CleKeLa
+//
+//  Created by Axel Lalaut on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CleKeLaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
